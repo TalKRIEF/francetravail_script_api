99784c9b63e36c6b48f27f6f4507efa2e499ea3c
Tal KRIEF <talkrief@gmail.com>
2025-10-23 21:17:33 +0200

    script working and filtering by latest offers creation date

--- autojob.exs
+++ autojob.exs
@@ -3,36 +3,47 @@ Mix.install([
   {:jason, "~> 1.4"}
 ])
 
-url = "https://candidat.francetravail.fr/gw-metierscope/job/M1855/offers"
+defmodule Offers do
+  def find_offers(department) do
+    url = "https://candidat.francetravail.fr/gw-metierscope/job/M1855/offers"
 
-response =
-  HTTPoison.get!(
-    url,
-    [
-      {"Referer",
-       "https://candidat.francetravail.fr/metierscope/fiche-metier/M1855/developpeur-developpeuse-web"}
-    ],
-    params: [
-      {"max", "150"}
-    ]
-  )
+    response =
+      HTTPoison.get!(
+        url,
+        [
+          {"Referer",
+           "https://candidat.francetravail.fr/metierscope/fiche-metier/M1855/developpeur-developpeuse-web"}
+        ],
+        params: [
+          {"max", "150"},
+          {"departmentNumber", department}
+        ]
+      )
 
-_filtered_data =
-  response.body
-  |> Jason.decode!()
-  |> Enum.filter(fn job -> job["codeDepartment"] in ["83", "13"] end)
-  |> Enum.each(fn offer ->
-    IO.puts("""
-    #{offer["title"]}
-    #{offer["location"]}
-    #{offer["company"]}
-    _______________________________________________________________________________
-    """)
-  end)
+    data =
+      response.body
+      |> Jason.decode!()
+      |> Enum.filter(fn job -> job["codeDepartment"] in [department] end)
+      |> Enum.sort_by(
+        fn job ->
+          {:ok, datetime} = NaiveDateTime.from_iso8601(job["creationDate"])
+          datetime
+        end,
+        {:desc, NaiveDateTime}
+      )
 
-# Enum.each(filtered_data, fn promise ->
-#   IO.puts("#{promise["title"]}")
-#   IO.puts("#{promise["location"]}")
-#   IO.puts("#{promise["company"]}")
-#   IO.puts("_______________________________________________________________________________________________________")
-# end)
+    Enum.each(data, fn offer ->
+      IO.puts("""
+      ********* #{offer["creationDate"]} #{offer["title"]} **********
+      #{offer["location"]}
+      #{offer["company"]}
+      _______________________________________________________________________________
+      """)
+    end)
+
+    data
+  end
+end
+
+Offers.find_offers("13")
+Offers.find_offers("83")
